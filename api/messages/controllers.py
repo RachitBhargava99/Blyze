efc0eba4eef40b6836fa4eef02b5a2645cfe8ed3
Steven Leone <stevenleone@lawn-143-215-90-234.lawn.gatech.edu>
2021-04-03 11:11:50 -0400

    finished individual messages

--- api/messages/controllers.py
+++ api/messages/controllers.py
@@ -8,6 +8,14 @@ from middleware.auth import AuthenticationMiddleware
 
 
 def get_messages(user_id: int):
-	chat_ids = db.query(models.)
+	#chat_ids = db.query(models.)
 	#db_chats = db.query(models.Chat).filter_by()
 	return db_chats
+
+def get_individual_messages(user_id: int):
+	chat_ids = db.query(models.Chat_Individual_Pairs).filter_by(user_id=user_id)
+	response = []
+	for chat_id in chat_ids:
+		messages = db.query(models.Message).filter_by(chat_id=chat_id)
+		response.append(messages)
+	return response
